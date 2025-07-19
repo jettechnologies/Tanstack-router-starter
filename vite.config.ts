6f7400b6122b9f12cbf802157cd1f0dc59cc9458
jettechnologies <tomesitime04@gmail.com>
2025-07-19 20:42:44 +0100

    chore: edited ts config to allow for import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@style": path.resolve(__dirname, "./src/style"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@containers": path.resolve(__dirname, "./src/containers"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@queries": path.resolve(__dirname, "./src/queries"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@mutations": path.resolve(__dirname, "./src/mutations"),
+    },
+  },
+});
